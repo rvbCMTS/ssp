1be451a5502ac89e7e23ef88ba5dfd72310c57c2
BwKodex <josef.lundman@gmail.com>
2018-07-16 15:35:00 +0200

    #29 Adds the missing mammography serializers

--- apps/mammography/serializers.py
+++ apps/mammography/serializers.py
@@ -1,15 +1,49 @@
 from rest_framework import serializers
-from .models import Modality, RoiValue
+
+from .models import Modality, Reference, MeasurementParameter, Measurement, RoiValue
 
 
-class ModalityMeasurementSerializer(serializers.Serializer):
+
+class ReferenceSerializer(serializers.Serializer):
+
+    class Meta:
+        model = Reference
+        fields = ('roi', 'mean', 'stdev', 'signal_noise_ratio')
+
+
+class MeasurementParameterSerializer(serializers.Serializer):
+    reference = ReferenceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = MeasurementParameter
+        fields = ('parameter')
+
+
+class RoiValueSerializer(serializers.Serializer):
+
+    class Meta:
+        model = RoiValue
+
+
+class MeasurementSerializer(serializers.Serializer):
+    roi_values = RoiValueSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Measurement
+        fields = ('modality', 'measurement_date', 'mas', 'entrance_dose', 'comment', 'signature')
+
+
+class ModalityReferenceSerializer(serializers.Serializer):
+    measurement_parameters = MeasurementParameterSerializer(many=True, read_only=True)
 
     class Meta:
         model = Modality
         fields = '__all__'
 
-class ModalityReferenceSerializer(serializers.Serializer):
+
+class ModalityMeasurementSerializer(serializers.Serializer):
+    measurements = MeasurementSerializer(many=True, read_only=True)
 
     class Meta:
-        model = RoiValue
+        model = Modality
         fields = '__all__'
