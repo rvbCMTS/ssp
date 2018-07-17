121de2c0ce94e4b5119eba89fcb17d13d5cdf3d1
psvenmarker <pontus.svenmarker@vll.se>
2018-07-17 13:15:02 +0200

    #31 ajax up and working

--- apps/skeleton_protocols/static/skeleton_protocols/sp.js
+++ apps/skeleton_protocols/static/skeleton_protocols/sp.js
@@ -3,4 +3,31 @@ $(document).ready(function() {
         paging: false,
         searching: false,
     } );
+
 });
+
+$("#form").change(function () {
+    console.log( $(this).val() );
+
+    $.ajax({
+        type: "GET",
+        url: $("#idUpdateResults").html(),
+        dataType: 'json',
+        data : $("#form").serialize(),
+        success: function(data) {
+            var table = $("#idResultTable").DataTable();
+            table.clear().rows.add(data.data).draw();
+        }
+    });
+
+});
+
+// hide and show spinner animation
+var $loading = $('#idLoaderRow').hide();
+$(document)
+  .ajaxStart(function () {
+    $loading.show();
+  })
+  .ajaxStop(function () {
+    $loading.hide();
+  });
