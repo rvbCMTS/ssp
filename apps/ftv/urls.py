b47688313b2be07d05636f220227537870b4f4d0
BwKodex <josef.lundman@gmail.com>
2018-09-18 15:58:21 +0200

    #39 Adds url for OP300 test

--- apps/ftv/urls.py
+++ apps/ftv/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import list_ftv_machines, MachineInfoList
+from .views import list_ftv_machines, MachineInfoList, op300_test_form
 
 
 app_name = 'ftv'
 urlpatterns = [
     path('', list_ftv_machines, name='machines'),
+    path('op300_test', op300_test_form, name='op300_form'),
     path('api/machine_info_list/', MachineInfoList.as_view(), name='machine_info_list')
 ]
