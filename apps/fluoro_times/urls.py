b7f6f548aee7d8a6aa6f2165c3c193e18e406a3b
BwKodex <josef.lundman@gmail.com>
2019-03-20 12:21:47 +0100

    #47 Fix fluoro time summary and update plotly

--- apps/fluoro_times/urls.py
+++ apps/fluoro_times/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import index, register_exam_form, api_save_new_exam, yearly_reports, ssm_plots, data_cleaning,\
-    api_save_clean_data, IndexSummaryData, api_update_orbit_data, YearlyReportData
+    api_save_clean_data, IndexSummaryData, api_update_orbit_data, YearlyReportData, SsmPlotData
 
 app_name = 'fluoro_times'
 urlpatterns = [
@@ -14,5 +14,6 @@ urlpatterns = [
     path('api/save_clean_data/', api_save_clean_data, name='save_clean_data'),
     path('api/dashboard_data/', IndexSummaryData.as_view(), name='dashboard_data'),
     path('api/yearly_report/', YearlyReportData.as_view(), name='yearly_report'),
+    path('api/ssm_plot/', SsmPlotData.as_view(), name='ssm_plot_api'),
     path('api/update_orbit/', api_update_orbit_data, name='update_orbit'),
 ]
